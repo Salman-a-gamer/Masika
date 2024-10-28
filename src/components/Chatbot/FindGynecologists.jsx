6fb1c4716c945eada10492012030c587bad547af
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2024-10-28 07:42:48 +0530

    Update FindGynecologists.jsx

--- src/components/Chatbot/FindGynecologists.jsx
+++ src/components/Chatbot/FindGynecologists.jsx
@@ -12,7 +12,7 @@ const FindGynecologists = () => {
       const geocodingResponse = await axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${location}&key=b5f7443436114367943cd2754a861b12`);
       const { lat, lng } = geocodingResponse.data.results[0].geometry;
 
-      const placesResponse = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=5000&type=doctor&keyword=gynecologist&key=AIzaSyAOVYRIgupAurZup5y1PRh8Ismb1A3lLao`);
+      const placesResponse = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=5000&type=doctor&keyword=gynecologist&key=APIKEY`);
       setGynecologists(placesResponse.data.results);
     } catch (error) {
       console.error('Error finding gynecologists:', error);
